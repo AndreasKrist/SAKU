2faaf742abee0f21a8c0c2f0300fa454b934d830
Andreas Kristianto <andreaskrist2004@gmail.com>
2026-01-07 01:17:46 +0700

    Update add_auto_equity_setting.sql

--- supabase/add_auto_equity_setting.sql
+++ supabase/add_auto_equity_setting.sql
@@ -5,4 +5,4 @@ ALTER TABLE businesses
 ADD COLUMN IF NOT EXISTS auto_update_equity_on_contribution BOOLEAN DEFAULT TRUE;
 
 COMMENT ON COLUMN businesses.auto_update_equity_on_contribution IS
-  'When true, equity percentages will automatically update based on capital contributions. When false, equity must be manually updated by owner.';
+  'Whens true, equity percentages will automatically update based on capital contributions. When false, equity must be manually updated by owner.';
